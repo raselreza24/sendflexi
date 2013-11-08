6eddd961557b296af97523955c07e8f3ee5817c3
Rasel Reza <raselreza24@gmail.com>
2013-11-09 02:39:57 +0600

    AppConfig.php

---
+++ AppConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_host 		= "localhost";
+
+$db_port 		= "";
+
+$db_user 		= "root";
+
+$db_password 	= "root";
+
+$db_name 		= "test";
+
+define("EXT", '.php');
+
+$secret = "sendflexi";  #Secuirity Salt
